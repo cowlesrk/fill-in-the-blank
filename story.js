0c290dedfc25e15400979f950ae13ca1c284c451
cowlesrk <cowlesrk@yahoo.com>
2017-04-05 16:47:38 -0400

    added story script

--- story.js
+++ story.js
@@ -3,4 +3,7 @@ var verb = prompt("Please type a verb:");
 var adjective = prompt("Please type an adjective:");
 var adverb = prompt("Please type an adverb:");
 
+alert("Ready to read your story?");
+var story = "This is a " + adjective + " " + noun + ". It likes to " + verb + " " + adverb + ".";
 
+document.write(story);
