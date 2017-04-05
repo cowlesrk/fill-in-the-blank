b18a0333cba11afcd6f11c4b471bbc8dff6a1500
cowlesrk <cowlesrk@yahoo.com>
2017-04-05 16:37:10 -0400

    added variables

---
+++ story.js
@@ -0,0 +1,6 @@
+var noun = prompt("Please type a noun:");
+var verb = prompt("Please type a verb:");
+var adjective = prompt("Please type an adjective:");
+var adverb = prompt("Please type an adverb:");
+
+
