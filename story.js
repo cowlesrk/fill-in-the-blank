5432ee1413de2a98c37a464606a8b1ff843e25b8
cowlesrk <cowlesrk@yahoo.com>
2017-04-05 16:48:37 -0400

    changed text to make more sense

--- story.js
+++ story.js
@@ -4,6 +4,6 @@ var adjective = prompt("Please type an adjective:");
 var adverb = prompt("Please type an adverb:");
 
 alert("Ready to read your story?");
-var story = "This is a " + adjective + " " + noun + ". It likes to " + verb + " " + adverb + ".";
+var story = "This is a " + adjective + " " + noun + ". It is " + verb + " " + adverb + ".";
 
 document.write(story);
