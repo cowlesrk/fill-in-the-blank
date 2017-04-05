5573178c62c498d5a24116352b4ee0eebefb4aae
cowlesrk <cowlesrk@yahoo.com>
2017-04-05 16:54:55 -0400

    made prompts more specific

--- story.js
+++ story.js
@@ -1,7 +1,7 @@
 var noun = prompt("Please type a noun:");
-var verb = prompt("Please type a verb:");
+var verb = prompt("Please type an -ing verb:");
 var adjective = prompt("Please type an adjective:");
-var adverb = prompt("Please type an adverb:");
+var adverb = prompt("Please type an adverb ending in -ly:");
 
 alert("Ready to read your story?");
 var story = "This is a " + adjective + " " + noun + ". It is " + verb + " " + adverb + ".";
