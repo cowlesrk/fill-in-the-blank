3b65ba2071c8d198b978bdc4c6c92e9f2200b3de
cowlesrk <cowlesrk@yahoo.com>
2017-04-07 11:58:35 -0400

    completed first random number challenge

--- story.js
+++ story.js
@@ -1,17 +1,25 @@
-var questions = 3;
-var questionsLeft = " [" + questions + " questions left]";
-var noun = prompt("Please type a noun:" + questionsLeft);
-questions -= 1;
-var questionsLeft = " [" + questions + " questions left]";
-var verb = prompt("Please type an -ing verb:" + questionsLeft);
-questions -= 1;
-var questionsLeft = " [" + questions + " questions left]";
-var adjective = prompt("Please type an adjective:" + questionsLeft);
-questions -= 1;
-var questionsLeft = " [" + questions + " questions left]";
-var adverb = prompt("Please type an adverb ending in -ly:");
+// var questions = 3;
+// var questionsLeft = " [" + questions + " questions left]";
+// var noun = prompt("Please type a noun:" + questionsLeft);
+// questions -= 1;
+// var questionsLeft = " [" + questions + " questions left]";
+// var verb = prompt("Please type an -ing verb:" + questionsLeft);
+// questions -= 1;
+// var questionsLeft = " [" + questions + " questions left]";
+// var adjective = prompt("Please type an adjective:" + questionsLeft);
+// questions -= 1;
+// var questionsLeft = " [" + questions + " questions left]";
+// var adverb = prompt("Please type an adverb ending in -ly:");
 
-alert("Ready to read your story?");
-var story = "This is a " + adjective + " " + noun + ". It is " + verb + " " + adverb + ".";
+// alert("Ready to read your story?");
+// var story = "This is a " + adjective + " " + noun + ". It is " + verb + " " + adverb + ".";
 
-document.write(story);
+// document.write(story);
+
+
+var input = prompt("Pick a number, any number!");
+var topNumber = parseInt(input);
+var randomNumber = Math.floor(Math.random() * topNumber) + 1;
+
+var message = randomNumber + " is a number between 1 and " + topNumber + " .";
+document.write(message);
