7cf8abcd90ff5d43b8abc19689c7f288e04f2006
cowlesrk <cowlesrk@yahoo.com>
2017-04-11 15:52:04 -0400

    completed conditional challenge solution

--- story.js
+++ story.js
@@ -44,24 +44,96 @@
 // Random Number Game //
 /////////////////////////
 
-var correctGuess = false;
-var randomNumber = Math.floor(Math.random() * 6) +1;
-var guess = prompt("I'm thinking of a number bewteen 1 and 6. What is it?");
-if (parseInt(guess) === randomNumber) {
-	correctGuess = true;
-} else if (parseInt(guess) < randomNumber) {
-	var guessMore = prompt("Try again. That number was too low.");
-		if (parseInt(guessMore) === randomNumber) {
-			correctGuess = true;
+// var correctGuess = false;
+// var randomNumber = Math.floor(Math.random() * 6) +1;
+// var guess = prompt("I'm thinking of a number bewteen 1 and 6. What is it?");
+// if (parseInt(guess) === randomNumber) {
+// 	correctGuess = true;
+// } else if (parseInt(guess) < randomNumber) {
+// 	var guessMore = prompt("Try again. That number was too low.");
+// 		if (parseInt(guessMore) === randomNumber) {
+// 			correctGuess = true;
+// 	}
+// } else if (parseInt(guess) > randomNumber) {
+// 	var guessLess = prompt("Try again. That number was too high.");
+// 		if (parseInt(guessLess) === randomNumber) {
+// 			correctGuess = true;
+// 	}
+// }	
+// if (correctGuess) {
+// 	document.write("You guessed the number! It was " + randomNumber + ".");
+// } else {
+// 	document.write("Sorry, the number was " + randomNumber + ".");
+// }
+
+/////////////////////////
+// Conditional Challenge //
+/////////////////////////
+
+// Challenge Instructions
+
+// Ask at least five questions
+
+// Keep track of the number of questions the user answered correctly
+
+// Provide a final message after the quiz letting the user know the number of questions he or she got right.
+
+// Rank the player. If the player answered all five correctly, 
+// give that player the gold crown: 3-4 is a silver crown; 
+// 1-2 correct answers is a bronze crown and 0 correct is no crown at all.
+
+///////////////////////////////
+
+// quiz begins, no answers correct
+
+var correct = 0;
+
+// ask questions
+
+var question1 = prompt("How old am I?");
+	if (parseInt(question1) === 25) {
+		correct += 1;
 	}
-} else if (parseInt(guess) > randomNumber) {
-	var guessLess = prompt("Try again. That number was too high.");
-		if (parseInt(guessLess) === randomNumber) {
-			correctGuess = true;
+
+var question2 = prompt("What is my favorite color?");
+	if (question2.toUpperCase() === "MINT") {
+		correct += 1;
 	}
-}	
-if (correctGuess) {
-	document.write("You guessed the number! It was " + randomNumber + ".");
+
+var question3 = prompt("What is my dog's name?");
+	if (question3.toUpperCase() === "KRYSTAL") {
+		correct += 1;
+	}
+
+var question4 = prompt("How many years have I lived in Charlotte?");
+	if (parseInt(question4) === 23) {
+		correct += 1;
+	}
+
+var question5 = prompt("What is my favorite fruit?");
+	if (question5.toUpperCase() === "MANGO" || "STRAWBERRY" || "STRAWBERRIES") {
+		correct += 1;
+	}
+
+// output results
+
+document.write("You got " + correct + " out of 5 questions correct.");
+
+// output results
+
+if ( correct === 5 ) {
+	document.write("You got a gold crown!");
+} else if ( correct >= 3) {
+	document.write("You got a silver crown!");
+} else if ( correct >= 1) {
+	document.write("You got a bronze crown!");
 } else {
-	document.write("Sorry, the number was " + randomNumber + ".");
+	document.write("You did not earn a crown. Better luck next time!");
 }
+
+
+
+
+
+
+
